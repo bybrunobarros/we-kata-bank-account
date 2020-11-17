af130b7b037c8150270580d811163e95e18254a4
bruno <bybrunobarros@gmail.com>
2020-11-17 15:28:33 +0100

    (unit) should return 401 when authentication token is not recognized ko

---
+++ app/auth/middleware.test.js
@@ -0,0 +1,26 @@
+import test from "ava";
+import { arrange } from "../../test/helper/arrange-middleware.js";
+import { authenticate } from "./middleware.js";
+
+const defaultGetRequest = {
+  method: "GET",
+  url: "/accounts/1/operations",
+  headers: {
+    Authorization: "Bearer QWxiZXJ0",
+  },
+  params: {
+    accountId: 1,
+  },
+  context: {},
+};
+
+test("should return 401 when authentication token is not recognized", async (t) => {
+  const { db, request, response, next } = await arrange(t, {
+    ...defaultGetRequest,
+    ...{ headers: { Authorization: "BadToken" } },
+  });
+
+  await authenticate(db)(request, response, next);
+
+  t.is(response._getStatusCode(), 401);
+});
