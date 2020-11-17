b78187c4e47bed3b83830af5eabeb0475e811028
bruno <bybrunobarros@gmail.com>
2020-11-17 22:08:18 +0100

    fix: typo

--- app/account/operation/route.test.js
+++ app/account/operation/route.test.js
@@ -33,12 +33,9 @@ test("should return 401 when authentication token is not recognized", async (t) 
 test("should return 403 when user id and account id don't match", async (t) => {
   await arrange(t);
 
-  const response = await fetch(
-    `${t.context.prefixUrl}/accounts/account2/operations`,
-    {
-      headers: { Authorization: "Bearer QWxiZXJ0" },
-    },
-  );
+  const response = await fetch(`${t.context.prefixUrl}/accounts/2/operations`, {
+    headers: { Authorization: "Bearer QWxiZXJ0" },
+  });
 
   t.is(response.status, 403);
 });
