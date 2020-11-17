022b8692129805f022a493db5fe57779a92766ad
bruno <bybrunobarros@gmail.com>
2020-11-17 11:31:37 +0100

    should add 20 to current balance when users make a deposit of 20 ok

--- app/account/operation/middleware.js
+++ app/account/operation/middleware.js
@@ -1,3 +1,23 @@
-export const operate = (/* db */) => (req, res) => {
-  res.json({});
+import { createOperation } from "./service.js";
+
+export const operate = (db) => async (req, res) => {
+  const { accountId, user } = req.context;
+  const { amount, type } = req.body;
+
+  const operation = await createOperation(db)({
+    accountId,
+    userId: user.id,
+    amount,
+    operationType: type,
+  });
+
+  res.json({
+    status: "succeed",
+    body: {
+      amount: operation.amount,
+      balance: operation.balance,
+      date: new Date(operation.created_at).toISOString().split("T")[0],
+      operation: operation.type,
+    },
+  });
 };
---
+++ app/account/operation/service.js
@@ -0,0 +1,33 @@
+const getLastOperation = (db) => async (userId, accountId) => {
+  const operations = await db("operation")
+    .where({
+      account_id: accountId,
+      user_id: userId,
+    })
+    .orderBy("id", "desc")
+    .limit(1);
+
+  return operations[0];
+};
+
+export const createOperation = (db) => async ({
+  userId,
+  accountId,
+  operationType,
+  amount,
+}) => {
+  const { balance: currentBalance } = await getLastOperation(db)(
+    userId,
+    accountId,
+  );
+
+  await db("operation").insert({
+    account_id: accountId,
+    user_id: userId,
+    type: operationType,
+    amount: amount,
+    balance: currentBalance + amount,
+  });
+
+  return getLastOperation(db)(userId, accountId);
+};
